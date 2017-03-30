bee078656acdf093b83c810185f9dcf78f5cdc6f
Kyriakos Giannakis <kerk12gr@gmail.com>
2017-03-31 01:43:46 +0300

    Added sleep(2) to wait for the arduino to initialize.

--- unlock.py
+++ unlock.py
@@ -1,7 +1,8 @@
 import serial
-
+from time import sleep
 ser = serial.Serial(port="/dev/ttyACM0", baudrate=9600)
-
+ser.isOpen()
+sleep(2)  # Wait for the arduino to be ready.
 ser.write("ENABLE")
 ser.close()
 exit()
