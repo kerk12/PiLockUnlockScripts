464126d3b4762b991740388f69ac613446416223
Kyriakos Giannakis <kerk12gr@gmail.com>
2017-04-15 21:43:39 +0300

    Defined the unlock script as a function, in order for it to be integrated with Django.

--- unlock.py
+++ unlock.py
@@ -1,8 +1,13 @@
 import serial
 from time import sleep
-ser = serial.Serial(port="/dev/ttyACM0", baudrate=9600)
-ser.isOpen()
-sleep(2)  # Wait for the arduino to be ready.
-ser.write("ENABLE")
-ser.close()
-exit()
+
+def unlock():
+    ser = serial.Serial(port="/dev/ttyACM0", baudrate=9600)
+    ser.isOpen()
+    sleep(2)  # Wait for the arduino to be ready.
+    ser.write("ENABLE")
+    ser.close()
+    exit()
+
+if __name__ == "__main__":
+    unlock()
