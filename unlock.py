82794ae6c732a56d0dba661bfbf5eb21cdd62103
Kyriakos Giannakis <kerk12gr@gmail.com>
2018-02-25 22:11:58 +0200

    Updated serial writing for Python 3 conversion.

--- unlock.py
+++ unlock.py
@@ -5,7 +5,7 @@ def unlock():
     ser = serial.Serial(port="/dev/ttyACM0", baudrate=9600)
     ser.isOpen()
     sleep(2)  # Wait for the arduino to be ready.
-    ser.write("ENABLE")
+    ser.write(b"ENABLE")
     ser.close()
 
 if __name__ == "__main__":
